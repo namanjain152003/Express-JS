9ea7e171ec78950bd639f0b301298e0b35603e78
NAMAN JAIN <namanjain152003@gmail.com>
2025-06-15 11:53:22 +0000

    http requests

--- HTTP-Requests/index.js
+++ HTTP-Requests/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 const app=express();
 const port =3000;
+
+// this is for acessing the root node or home page
 app.get("/",(req,res)=>{
-    // console.log(req.rawHeaders);
-     res.send("helloo")
+     console.log(req.rawHeaders); // rawHeaders are the key value pairs that come from server
+     res.send("helloo naman")
  })
+ // req is request and res is response
+
+ // this is about page
  app.get("/about",(req,res)=>{
     // console.log(req.rawHeaders);
      res.send("hell ")
