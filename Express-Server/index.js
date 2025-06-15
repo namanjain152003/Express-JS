8ad9e6ab5bb1cbc63f3d56d5a80bcc3d3a3db0c8
NAMAN JAIN <namanjain152003@gmail.com>
2025-06-15 08:57:59 +0000

    express js

--- Express-Server/index.js
+++ Express-Server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app=express();
 const port=3000; 
 app.listen(port,()=> {
-    console.log(`Server rumming on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
 
 // here ()=> is a callback function which tells about the successfull establishment of the server.
